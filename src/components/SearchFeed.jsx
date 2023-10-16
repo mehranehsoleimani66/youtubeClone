532c189b458e2ecd0f96fc31062cbfc76053d644
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-17 00:51:35 +0330

    createdSearchfeed.jsx and sidebar.jsx

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Typography, Stack, Box } from "@mui/material";
+
+const SearchFeed = () => {
+  return (
+    
+  );
+};
+
+export default SearchFeed;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+const SideBar = () => {
+  const categorySelected = "New";
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" }
+      }}
+    >
+      {categories.map((category) => (
+        <button
+          className="category-btn"
+          style={{
+            background: category.name === categorySelected && "#FC1503"
+          }}
+        >
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+          <span></span>
+          <span></span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default SideBar;
