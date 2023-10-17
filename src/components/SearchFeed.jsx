b58a4e949da14fd62e477d66ada3c52ca7ce366c
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-17 14:45:45 +0330

    modified

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -3,9 +3,7 @@ import { useState, useEffect } from "react";
 import { Typography, Stack, Box } from "@mui/material";
 
 const SearchFeed = () => {
-  return (
-    
-  );
+  return;
 };
 
 export default SearchFeed;
