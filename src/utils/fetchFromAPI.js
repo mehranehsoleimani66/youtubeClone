2e549d9d4dbc47043cf66bb919d6293c3f32a890
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-19 15:38:53 +0330

    change the codes due to change the api

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,11 +1,27 @@
+// import axios from "axios";
+
+// export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+// const options = {
+//   params: {
+//     maxResults: 50
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+//     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com"
+//   }
+// };
+
+// export const fetchFromAPI = async (url) => {
+//   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+//   return data;
+// };
 import axios from "axios";
-
 export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
-
 const options = {
-  url: BASE_URL,
   params: {
-    maxResults: 50
+    maxResults: "10"
   },
   headers: {
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
@@ -18,3 +34,10 @@ export const fetchFromAPI = async (url) => {
 
   return data;
 };
+
+// try {
+//   const response = await axios.request(options);
+//   console.log(response.data);
+// } catch (error) {
+//   console.error(error);
+// }
